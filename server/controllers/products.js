b9cde9b7bf08397ded474a37ec5561e2916a86cd
Abdullatif Khamis <168638272+TitanTesla@users.noreply.github.com>
2025-04-11 23:33:47 +0400

    updated multer configuration for image path

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -5,7 +5,7 @@ const path = require('path');
 // Configure multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'client/images/');
+    cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
